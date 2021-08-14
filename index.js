123a9e028188f8fae205def6f9bde01edde861a7
Naveen0610 <naveenmmech066@gmail.com>
2021-08-14 10:36:24 +0530

    added es6

--- index.js
+++ index.js
@@ -74,3 +74,104 @@ for(i=0;i<=student.hobbies.lenght-1;i++)
 {
     console.log(student.hobbies[i]);
 }
+// let and const
+//var --> can be hosted,
+// let and const cant't be hosted , let ----> variable , const --->constants
+// let and const block scoped variable
+// var -->it's not 
+
+
+function vartest(){
+    var x =1;
+    if(true) {
+        var x =2;
+        console.log(x);
+    }
+    console.log(x);
+}
+vartest()
+
+function es6test() {
+    let x =1;
+    if(true) {
+        let x=2;
+        console.log(x);
+        }
+        console.log(x);
+}
+es6test()
+
+
+// functions
+function add(a,b){
+    return a + b;
+}
+add(3,5);
+
+//FE
+var add1 = function(a,b) {
+    return a + b;
+}
+add1(6,2);
+
+//es6 arrow functions
+const add2 = (a,b) => {
+    return a + b;
+}
+
+//one statement
+const add3 = (a,b) => a + b;
+const mul =(a,b) => a * b;
+var res = mul(3,3);
+console.log(res);
+
+
+//default parameters values
+function demo(x,y,z){
+    if(y=== undefined){
+        y=0;
+    }
+    if (z=== undefined){
+        z=0;
+    }
+    return x + y + z;
+};
+let res1 = demo(15,25);
+
+const demo2 =(a, b=10, c=30) => {
+    return a + b + c;
+}
+const result = demo2(10);
+console.log(result);
+
+//string concatination
+var address = ' To All';
+var greeting = 'Happy independence day';
+var message = greeting + address ;
+console.log(message);
+
+
+//templates literals
+const mes =`${greetings} ${address}`
+console.log(mes);
+
+//arrays
+const arr =[3,5,6,1];
+const arry2 =[];
+for(let i=0; i<=arr.length-1; i++) {
+    arry2.push(arr[i] * 2)
+}
+console.log(arry2);
+
+// array helper method 
+//map, find, filter, foreach and reduce
+const mul2 = arr.map(function(e){
+    console.log(e)
+})
+const mul3 = arr.map((item) => {
+    return item * 2
+})
+
+console.log(arr)
+console.log(mul3)
+
